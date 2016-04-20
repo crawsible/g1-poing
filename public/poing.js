b358877a34bf9c7f46c81029cf79a02a6625db8d
Colin Jackson <colin.s.jackson90@gmail.com>
2016-04-20 00:58:45 -0400

    Add latching behavior on collision without immobility

--- public/poing.js
+++ public/poing.js
@@ -12,19 +12,15 @@ function preload() {}
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-
   paddles = game.add.group();
 
   var paddleGraphics = new Phaser.Graphics(null, 0, 0);
   paddleGraphics.beginFill(0xFFFF88, 1);
   paddleGraphics.drawRect(0, 0, 10, 50);
   var paddleTexture = paddleGraphics.generateTexture();
-  p1Paddle = paddles.create(20, 125, paddleTexture);
-  p1Paddle.immobilityFrames = IMMOBILITY_FRAMES;
-  p1Paddle.immobilityRecharge = IMMOBILITY_RECHARGE;
-  p2Paddle = paddles.create(570, 125, paddleTexture);
-  p2Paddle.immobilityFrames = IMMOBILITY_FRAMES;
-  p2Paddle.immobilityRecharge = IMMOBILITY_RECHARGE;
+
+  p1Paddle = createPaddle(245, 125, 1, paddles, paddleTexture);
+  p2Paddle = createPaddle(345, 125, -1, paddles, paddleTexture);
 
   var ballGraphics = new Phaser.Graphics(null, 0, 0);
   ballGraphics.beginFill(0xFFFFFF, 1);
@@ -40,7 +36,6 @@ function create() {
 
   ball.enableBody = true;
   ball.body.bounce.x = 1.1;
-  ball.body.bounce.y = 1;
   ball.body.collideWorldBounds = true;
 
   ball.body.velocity.x = 50;
@@ -50,10 +45,25 @@ function update() {
   handleImmobility(p1Paddle, Phaser.KeyCode.Z);
   handleImmobility(p2Paddle, Phaser.KeyCode.PERIOD);
 
-  game.physics.arcade.collide(ball, paddles);
+  game.physics.arcade.collide(ball, paddles, function(ball, paddle) {
+    if (paddle.immovable) {
+      ball.body.velocity.x = paddle.body.velocity.x;
+      paddle.capturedBall = ball;
+    }
+  });
 }
 
 // private methods
+
+function createPaddle(x, y, direction, paddles, texture) {
+  var paddle = paddles.create(x, y, texture);
+  paddle.immobilityFrames = IMMOBILITY_FRAMES;
+  paddle.immobilityRecharge = IMMOBILITY_RECHARGE;
+  paddle.direction = direction;
+
+  return paddle;
+}
+
 function handleImmobility(paddle, key) {
   paddle.body.immovable = false;
   paddle.tint = 0xFFFFFF;
@@ -74,26 +84,13 @@ function handleImmobility(paddle, key) {
     paddle.body.velocity.x = 0;
     paddle.body.immovable = true;
     paddle.tint = 0xFF0000;
-  } else if (paddle.immobilityFrames < IMMOBILITY_FRAMES) {
-    paddle.immobilityFrames += 1;
+  } else {
+    if (paddle.immobilityFrames < IMMOBILITY_FRAMES) {
+      paddle.immobilityFrames += 1;
+    }
+
+    if (paddle.capturedBall) {
+      ball.body.velocity.x = 10 * paddle.direction;
+    }
   }
-}
-
-function checkForActivation(paddle, key) {
-
-  //if (game.input.keyboard.isDown(key) && handleImmobilityRequest(paddle)) {
-    //paddle.body.velocity = 0;
-    //paddle.body.immovable = true;
-    //paddle.tint = 0xFF0000;
-  //}
-}
-
-function handleImmobilityRequest(paddle) {
-  if (paddle.immobilityFrames <= 0) {
-    paddle.immobilityFrames = IMMOBILITY_FRAMES;
-    return false;
-  }
-
-  paddle.immobilityFrames -= 1;
-  return true;
 }
